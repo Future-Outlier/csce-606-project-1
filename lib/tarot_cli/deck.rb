e5c3f36749980312cb05e38e93816844e2ab5c22
Ian Beckett <ianedwardbeckett@gmail.com>
2026-09-07 23:19:12 -0500

    test: Enable dependency injection for Deck

--- lib/tarot_cli/deck.rb
+++ lib/tarot_cli/deck.rb
@@ -7,8 +7,8 @@ FILE_PATH = File.expand_path('../data/cards.json', __dir__)
 class Deck
   attr_reader :drawn_cards
 
-  def initialize
-    @cards = Card.load_from_file(FILE_PATH)
+  def initialize(cards: nil)
+    @cards = cards || Card.load_from_file(FILE_PATH)
     @drawn_cards = []
   end
 

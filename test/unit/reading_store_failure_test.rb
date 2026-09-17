3b8e924e32b8f504bd6bcc48dd6b7510f46140aa
Ian Beckett <ianedwardbeckett@gmail.com>
2026-09-17 00:11:33 -0500

    Reduce redundant save failure tests
    Signed-off-by: Ian Beckett <ianedwardbeckett@gmail.com>

--- test/unit/reading_store_failure_test.rb
+++ test/unit/reading_store_failure_test.rb
@@ -23,52 +23,11 @@ class ReadingStoreFailureTest < Minitest::Test
     end
   end
 
-  def test_rejects_invalid_history_containers
-    ['null', '[]', '{}', '{"readings": null}', '{"readings": {}}', '{"readings": [null]}'].each do |contents|
-      assert_rejected_history(contents)
-    end
-  end
-
-  def test_rejects_invalid_record_fields
-    save_reading
-    reading = JSON.parse(File.read(@path))['readings'].first
-    invalid_fields = { 'ID' => '1', 'saved_at' => nil, 'question' => nil,
-                       'cards' => [123], 'interpretation' => nil }
-    invalid_fields.each do |key, value|
-      assert_rejected_history(JSON.generate('readings' => [reading.merge(key => value)]))
-    end
-  end
-
-  def test_rejects_duplicate_ids
-    save_reading
-    reading = JSON.parse(File.read(@path))['readings'].first
-
-    assert_rejected_history(JSON.generate('readings' => [reading, reading]))
-  end
-
   def test_reports_an_unwritable_destination
     store = TarotCLI::ReadingStore.new(path: File.join(@directory, 'missing', 'readings.json'))
 
     assert_raises(TarotCLI::ReadingStore::Error) { store.save(question: 'Question', cards: ['The Fool']) }
     assert_empty Dir.children(@directory)
-  end
-
-  def test_reports_a_directory_used_as_a_file
-    Dir.mkdir(@path)
-
-    assert_raises(TarotCLI::ReadingStore::Error) { save_reading }
-    assert File.directory?(@path)
-  end
-
-  def test_failed_serialization_preserves_history_and_removes_the_temporary_file
-    save_reading
-    original = File.binread(@path)
-
-    assert_raises(TarotCLI::ReadingStore::Error) do
-      @store.save(question: "\xFF".dup.force_encoding(Encoding::UTF_8), cards: ['The World'])
-    end
-    assert_equal original, File.binread(@path)
-    assert_equal ['readings.json'], Dir.children(@directory)
   end
 
   private
